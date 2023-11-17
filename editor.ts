068c99dfc8e022d6854bb968f537103f289fb518
kashikichi <60090083+Pikselas@users.noreply.github.com>
2023-11-17 15:59:14 +0530

    change color icon added

--- editor.ts
+++ editor.ts
@@ -5,24 +5,29 @@ class Draggable
    private drag_handler: any;
    private drag_start: any;
    private drag_end: any;
-   constructor (capture_element: HTMLElement , target_element: HTMLElement)
+   private offset_x: number;
+   private offset_y: number;
+   constructor (capture_element: HTMLElement , target_element: HTMLElement , offset_x: number = 0, offset_y: number = 0)
    {
       this.drag_element = target_element;
       this.drag_start = this.onDragStart.bind(this);
       this.drag_end = this.onDragEnd.bind(this);
+
+      this.offset_x = offset_x;
+      this.offset_y = offset_y;
       
       capture_element.addEventListener("mousedown", this.drag_start);
       capture_element.addEventListener("mouseup", this.drag_end);
    }
    private onDragStart(event: any): void
    {
-      this.drag_handler = this.onDrag.bind(this,event.layerX, event.layerY);
+      this.drag_handler = this.onDrag.bind(this,event.layerX + this.offset_x, event.layerY + this.offset_y);
       this.drag_element.addEventListener("mousemove", this.drag_handler);
    }
    private onDrag(offset_x:number , offset_y:number, event:MouseEvent): void
    {
-     this.drag_element.style.left = (event.pageX - offset_x) - 10 + "px";
-     this.drag_element.style.top = (event.pageY - offset_y) - 10 + "px";
+     this.drag_element.style.left = (event.pageX - offset_x) + "px";
+     this.drag_element.style.top = (event.pageY - offset_y) + "px";
    }
    private onDragEnd(): void
    {
@@ -89,14 +94,14 @@ class Panel extends Draggable
    {
       const panel = document.createElement("div");
       const toolbar = new Toolbar();
-      super(toolbar.getToolbar(),panel);
+      super(toolbar.getToolbar(),panel , 10 , 10);
       this.panel = panel;
       this.toolbar = toolbar;
       this.panel.className = "panel";
       this.panel.appendChild(this.toolbar.getToolbar());
 
       this.toolbar.addTool(new Tool("./media/plus-small.png","Add Item"));
-      this.toolbar.addTool(new Tool("./media/plus-small.png","Remove Item"));
+      this.toolbar.addTool(new Tool("./media/color.png","Change Color"));
       this.toolbar.addTool(new Tool("./media/plus-small.png","Add Item"));
       //this.toolbar.addTool(new Tool("./media/plus-small.png","Remove Item"));
    }
--- js/editor.js
+++ js/editor.js
@@ -3,20 +3,24 @@ class Draggable {
     drag_handler;
     drag_start;
     drag_end;
-    constructor(capture_element, target_element) {
+    offset_x;
+    offset_y;
+    constructor(capture_element, target_element, offset_x = 0, offset_y = 0) {
         this.drag_element = target_element;
         this.drag_start = this.onDragStart.bind(this);
         this.drag_end = this.onDragEnd.bind(this);
+        this.offset_x = offset_x;
+        this.offset_y = offset_y;
         capture_element.addEventListener("mousedown", this.drag_start);
         capture_element.addEventListener("mouseup", this.drag_end);
     }
     onDragStart(event) {
-        this.drag_handler = this.onDrag.bind(this, event.layerX, event.layerY);
+        this.drag_handler = this.onDrag.bind(this, event.layerX + this.offset_x, event.layerY + this.offset_y);
         this.drag_element.addEventListener("mousemove", this.drag_handler);
     }
     onDrag(offset_x, offset_y, event) {
-        this.drag_element.style.left = (event.pageX - offset_x) - 10 + "px";
-        this.drag_element.style.top = (event.pageY - offset_y) - 10 + "px";
+        this.drag_element.style.left = (event.pageX - offset_x) + "px";
+        this.drag_element.style.top = (event.pageY - offset_y) + "px";
     }
     onDragEnd() {
         this.drag_element.removeEventListener("mousemove", this.drag_handler);
@@ -66,13 +70,13 @@ class Panel extends Draggable {
     constructor() {
         const panel = document.createElement("div");
         const toolbar = new Toolbar();
-        super(toolbar.getToolbar(), panel);
+        super(toolbar.getToolbar(), panel, 10, 10);
         this.panel = panel;
         this.toolbar = toolbar;
         this.panel.className = "panel";
         this.panel.appendChild(this.toolbar.getToolbar());
         this.toolbar.addTool(new Tool("./media/plus-small.png", "Add Item"));
-        this.toolbar.addTool(new Tool("./media/plus-small.png", "Remove Item"));
+        this.toolbar.addTool(new Tool("./media/color.png", "Change Color"));
         this.toolbar.addTool(new Tool("./media/plus-small.png", "Add Item"));
         //this.toolbar.addTool(new Tool("./media/plus-small.png","Remove Item"));
     }
